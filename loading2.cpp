ec00f6bb73dec387eb5019ca7c3e9e31f90bcf3f
Antoine Mirloup <antoine.mirloup@gmail.com>
2024-10-16 14:48:38 +0200

    amirloup 2024-10-16 14:48:38 2024-10-16 14:48:38

--- loading2.cpp
+++ loading2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+#include <cstdlib>
 #define NC		std::string("\e[0m")
 #define RED		std::string("\e[1;31m")
 #define GREEN	std::string("\e[1;32m")
@@ -8,24 +9,82 @@
 #define PURPLE	std::string("\e[1;35m")
 #define BLUE	std::string("\e[1;36m")
 
-int main()
+std::string create_string(int i, int size, bool rev, int nbr_of_triangle)
 {
-	int size = 10;
 	std::string t1 = RED + "▷";
 	std::string t2 = BLUE + "◀";
+	std::string t3 = GREEN + "▼";
+	std::string t4 = GREEN + "▲";
 	std::string buffer =  GREEN + "Loading..." + YELLOW + "[";
 	for (int j = 0; j < size; j++)
-		buffer += " ";
-	buffer += YELLOW + "]" + NC;
-	std::cout << buffer;
-	std::cout.flush();
-	for (int i = 12; i < (12 + size); i++)
 	{
-		buffer.replace(i, 10, t1);
-		std::cout << "\r" << buffer;
-		std::cout.flush();
-		sleep(1);
+		if (j < nbr_of_triangle)
+		{
+			if (j % 2 == 0)
+				buffer += t3;
+			else
+				buffer += t4;
+		}
+		else if (rev == 0)
+		{
+			if (i == j)
+				buffer += t1;
+			else
+				buffer += " ";
+		}
+		else
+		{
+			if (i == j)
+				buffer += t2;
+			else
+				buffer += " ";
+		}
 	}
+	buffer += YELLOW + "]" + NC;
+	return buffer;
+}
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	int size = atol(argv[1]);
+	int i = 0;
+	int r = 0;
+	bool reverse = 0;
+	int nbr_of_triangle = 0;
+	while (i < size)
+	{
+		while (1)
+		{
+			std::cout.flush();
+			std::cout << "\r";
+			if (r >= size)
+				std::cout << create_string(r, size, reverse, nbr_of_triangle);
+			else
+				std::cout << create_string(r, size, reverse, nbr_of_triangle);
+			if (size <= 10)
+				usleep(20000);
+			else if (size > 10 && size < 20)
+				usleep(5000);
+			else if (size > 20)
+				usleep(1000);
+			if (r < size && reverse == 0)
+				r++;
+			else
+			{
+				reverse = 1;
+				r--;
+			}
+			if (!r)
+				break;
+		}
+		reverse = 0;
+		nbr_of_triangle++;
+		i++;
+	}
+	std::cout.flush();
+	std::cout << "\r";
+	std::cout << create_string(r, size, reverse, nbr_of_triangle);
 	std::cout << std::endl;
 }
 
