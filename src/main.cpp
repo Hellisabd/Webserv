27290052e13eb22f1eaad47974473ee8b6eda087
Basile Grosjean <bgrosjea@k1r2p5.42mulhouse.fr>
2024-10-25 14:43:15 +0200

    bgrosjea 2024-10-25 14:43:15 reset

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,5 @@
 #include "webserv.hpp"
 
-
-void test(Data &data)
-{
-	int server_fd = socket(AF_UNSPEC, SOCK_STREAM, 0);
-	if (server_fd < 0)
-		throw Error("error while  creating socket");
-	struct sockaddr_in address;
-
-
-	
-
-}
-
 int main(int argc, char **argv)
 {
 	try
@@ -20,7 +7,6 @@ int main(int argc, char **argv)
 		if (argc != 2)
 			Error("Wrong number of arguments");
 		Data data(argv[1]);
-		test(data);
 	}
 	catch (std::exception const &e)
 	{
